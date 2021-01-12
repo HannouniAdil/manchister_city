98e7e53cfeda0d12f5a21208092281975d174391
adilhannouni <adilhannouni@gmail.com>
2021-01-12 00:16:49 -0700

    Add Header and logo component and toolbar links

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Routes from './routes';
+import './Resources/css/app.css';
+import { BrowserRouter } from 'react-router-dom';
 
+const App = () => {
+  return(
+    <BrowserRouter>
+     <Routes />
+  </BrowserRouter>
+  )
+}
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App/>,
   document.getElementById('root')
 );
